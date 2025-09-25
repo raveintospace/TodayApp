597c82c839e401269064ac5260a717d7ff1faf4a
Oriol Angelet <uriaeri@gmail.com>
2025-09-25 18:00:37 +0200

    Initial setup of the ProgressHeaderView for Progress Circle

---
+++ Today/Today/ListViewController/ProgressHeaderView.swift
@@ -0,0 +1,87 @@
+//
+//  ProgressHeaderView.swift
+//  Today
+//
+//  Created by Uri on 25/9/25.
+//
+
+import UIKit
+
+class ProgressHeaderView: UICollectionReusableView {
+    var progress: CGFloat = 0 {
+        // Updates the height constraint of the lower view when the value of progress changes
+        didSet {
+            heightConstraint?.constant = progress * bounds.height
+            UIView.animate(withDuration: 0.2) { [weak self] in
+                self?.layoutIfNeeded()
+            }
+        }
+    }
+        
+    private let upperView = UIView(frame: .zero)
+    private let lowerView = UIView(frame: .zero)
+    private let containerView = UIView(frame: .zero)
+    private var heightConstraint: NSLayoutConstraint?
+    
+    // Override to perform a custom initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        prepareSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // Override to adjust the corner radius of ProgressHeaderView whenever its size changes
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        containerView.layer.masksToBounds = true
+        containerView.layer.cornerRadius = 0.5 * containerView.bounds.width     // circle shape
+    }
+    
+    private func prepareSubviews() {
+        containerView.addSubview(upperView)
+        containerView.addSubview(lowerView)
+        addSubview(containerView)   // subview of ProgressHeaderView
+        
+        upperView.translatesAutoresizingMaskIntoConstraints = false
+        lowerView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        heightAnchor.constraint(equalTo: widthAnchor, multiplier: 1).isActive = true // ProgressHeaderView
+        
+        containerView.heightAnchor.constraint(equalTo: containerView.widthAnchor, multiplier: 1).isActive = true
+        containerView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        containerView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        containerView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.85).isActive = true
+        
+        upperView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        upperView.bottomAnchor.constraint(equalTo: lowerView.topAnchor).isActive = true
+        lowerView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        
+        upperView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        upperView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        lowerView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        lowerView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        
+        // This constraint increases the lower view’s height to reflect a user’s progress in completing reminders.
+        // As this constraint increases, the height of the upper view decreases because the heights are inversely proportional.
+        heightConstraint = lowerView.heightAnchor.constraint(equalToConstant: 0)
+        heightConstraint?.isActive = true
+        
+        backgroundColor = .clear
+        containerView.backgroundColor = .clear
+        upperView.backgroundColor = .todayProgressUpperBackground
+        lowerView.backgroundColor = .todayProgressLowerBackground
+    }
+}
+
+
+/*
+ Instead of deleting views when a user scrolls them out of the visible bounds, the UICollectionReusableView class keeps views in the reuse queue.
+ They are ideal for headers or footers
+ */
+
+
+// Friday - Section 3:  https://developer.apple.com/tutorials/app-dev-training/creating-a-progress-view

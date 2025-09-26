b7d6b39c24a51db562840d486442b6aa6e6d6204
Oriol Angelet <uriaeri@gmail.com>
2025-09-26 12:46:25 +0200

    Added a method to display alerts with custom errors in ReminderListView

--- Today/Today/ListViewController/ReminderListViewController.swift
+++ Today/Today/ListViewController/ReminderListViewController.swift
@@ -140,6 +140,16 @@ class ReminderListViewController: UICollectionViewController {
         backgroundView.layer.addSublayer(gradientLayer)
         collectionView.backgroundView = backgroundView
     }
-
+    
+    // Shows errors for the known issues
+    private func showError(_ error: Error) {
+        let alertTitle = NSLocalizedString("Error", comment: "Error alert title")
+        let alert = UIAlertController(title: alertTitle, message: error.localizedDescription, preferredStyle: .alert)
+        let actionTitle = NSLocalizedString("OK", comment: "Alert OK button title")
+        alert.addAction(UIAlertAction(title: actionTitle, style: .default, handler: { [weak self] _ in
+            self?.dismiss(animated: true)
+        }))
+        present(alert, animated: true, completion: nil)
+    }
 }
 

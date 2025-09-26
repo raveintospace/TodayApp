16ea3362b88dfe74e53aa0d73b7000a83020e008
Oriol Angelet <uriaeri@gmail.com>
2025-09-26 12:34:55 +0200

    Added method to obtain EKReminders and store them as Today Reminder

--- Today/Today/Models/ReminderStore.swift
+++ Today/Today/Models/ReminderStore.swift
@@ -16,6 +16,24 @@ final class ReminderStore {
     var isAvailable: Bool {
         EKEventStore.authorizationStatus(for: .reminder) == .fullAccess
     }
+    
+    /// Obtains user's EKReminders and stores them as custom Reminder for Today app
+    func readAll() async throws -> [Reminder] {
+        guard isAvailable else {
+            throw TodayError.accessDenied
+        }
+        
+        let predicate = ekStore.predicateForReminders(in: nil) // identifies all the reminders in a collection of calendars
+        let ekReminders = try await ekStore.reminders(matching: predicate)
+        let reminders: [Reminder] = try ekReminders.compactMap { ekReminder in
+            do {
+                return try Reminder(with: ekReminder)
+            } catch TodayError.reminderHasNoDueDate {
+                return nil
+            }
+        }
+        return reminders
+    }
 }
 
 /*
